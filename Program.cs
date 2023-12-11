4c7baa9f5d3385f17100b5fee0c31feb455dfc20
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-12-11 00:05:55 -0800

    Try again tomorrow

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Security.AccessControl;
+
+
+
+class Program {
+
+    static void Main(string [] arg) {
+
+      var azharhistory = new Player("Azhar Kimanje", 19); // call the class
+
+      Console.WriteLine($"{azharhistory.name} plays No. {azharhistory.position} for the stars"); // testing our function
+    }
+
+}
+
+class Player{ //class object implementation
+    public string name { get; set; }
+    public int position { get; set; } 
+    public int Muys{ get; set; }
+   public Player(string names, int positions) { // constructor
+    this.name = names;
+    position = positions;
+   }
+ }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Class Practice for C#
  Abstract Data Types 
+
+ Learning how to set up an ADT;
+
+ Learning how to use functionality of a simple ADT
